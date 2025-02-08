660206ffd3a5032b0cc8b7aee4b62d9cbc6199ec
Francisco Wesley <fwesleyper@gmail.com>
2025-02-08 15:33:18 -0300

    change lambda description

--- iac/main.tf
+++ iac/main.tf
@@ -6,7 +6,7 @@ module "tech-challenge-register-lambda" {
   lambda_function_name         = "tech-challenge-register-lambda"
   lambda_memory_size           = 128
   lambda_timeout               = 30
-  lambda_description           = "Lambda function to register lambda functions"
+  lambda_description           = "Lambda function to register lambda functions in the system"
   lambda_image_uri             = var.lambda_image_uri
   lambda_tags                  = local.tags
   lambda_package_type          = "Image"
